5a88842338dab08237234ab4252431dddf375941
Jatin Sonavane <sonavane@gmail.com>
2011-10-12 09:25:45 -0700

    Removed some unused code

--- Juggl/src/me/juggl/shared/WorkStream.java
+++ Juggl/src/me/juggl/shared/WorkStream.java
@@ -6,20 +6,22 @@ import java.io.Serializable;
 public class WorkStream implements Serializable {
 
 	private static final long serialVersionUID = -7993839882410507563L;
-
-	private String name = "";
 	
-	public WorkStream() {}
+	private long lastUpdateTime;
+	private String name;
+	
+	public WorkStream() {} // GWT needs a default constructor
 	
 	public WorkStream(String name) {
 		this.name = name;
+		lastUpdateTime = System.currentTimeMillis();
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public long getLastUpdateTime() {
+		return lastUpdateTime;
 	}
 }
--- Juggl/test/me/juggl/WorkStreamTest.java
+++ Juggl/test/me/juggl/WorkStreamTest.java
@@ -11,36 +11,15 @@ import org.junit.Test;
 public class WorkStreamTest {
 
 	@Test
-	public void defaultConstruction() {
+	public void construction() {
 		// Arrange
-		WorkStream subject = new WorkStream();
-		String expected = "";
+		long timeBefore = System.currentTimeMillis();
+		WorkStream subject = new WorkStream("test name");
+		long timeAfter = System.currentTimeMillis();
 		
 		// Assert
-		assertEquals(expected, subject.getName());
+		assertEquals("test name", subject.getName());
+		assertTrue(subject.getLastUpdateTime() <= timeAfter && subject.getLastUpdateTime() >= timeBefore);
 		assertTrue(subject instanceof Serializable); // GWT requirement when sharing between client and server
-	}
-	
-	@Test
-	public void constructionWithName() {
-		// Arrange
-		String name = "this is a test workstream";
-		WorkStream subject = new WorkStream(name);
-
-		// Assert
-		assertEquals(name, subject.getName());
-	}
-	
-	@Test
-	public void setName() {
-		// Arrange
-		WorkStream subject = new WorkStream("old name");
-		String expected = "new name";
-		
-		// Act
-		subject.setName("new name");
-
-		// Assert
-		assertEquals(expected, subject.getName());
 	}
 }
